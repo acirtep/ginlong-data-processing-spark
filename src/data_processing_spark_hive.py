d0337e9d64092df79bbf973f51ce7ad5422af9f2
Acirtep <petrica.leuca@gmail.com>
2022-07-22 13:21:54 +0200

    Provide dummy data

--- src/data_processing_spark_hive.py
+++ src/data_processing_spark_hive.py
@@ -1,10 +1,9 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import (
-    input_file_name, lit, col, split, regexp_replace, row_number, to_date
-)
-from pyspark.sql import Window
-from data_processing_spark import get_raw_electricity_data, data_transformation
 from uuid import uuid4
+from datetime import date, datetime
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructField, StructType, DateType, TimestampType, DoubleType
+
 
 def get_spark_session():
     """
@@ -12,16 +11,33 @@ def get_spark_session():
         SparkSession: spark session with the spark-excel package
     """
     return SparkSession.builder.appName("My Spark ETL Session").config(
-        'spark.jars.packages', 'com.crealytics:spark-excel_2.12:3.2.1_0.17.0'
-    ).config(
     'spark.hive.metastore.uris', 'thrift://localhost:9083'
     ).enableHiveSupport().getOrCreate()
 
 
+def get_some_df(spark_session):
+
+    records = [
+        [date(2022, 5, 3), datetime(2022, 5, 3, 21, 36, 3), 4.7],
+        [date(2022, 7, 14), datetime(2022, 7, 14, 22, 30, 6), 18.8],
+        [date(2022, 7, 15), datetime(2022, 7, 15, 22, 27, 30), 21.4],
+        [date(2022, 7, 16), datetime(2022, 7, 16, 22, 28, 36), 23.5],
+        [date(2022, 7, 17), datetime(2022, 7, 17, 22, 26, 15), 22.1]
+    ]
+    schema = StructType(
+        [
+            StructField('reporting_date',DateType(),True),
+            StructField('time',TimestampType(),True),
+            StructField('produced_kwh',DoubleType(),True)
+        ]
+    )
+
+    return spark_session.createDataFrame(records, schema)
+
+
 if __name__ == "__main__":
     spark_session = get_spark_session()
-    input_df = get_raw_electricity_data(spark_session, "*")
-    data_to_be_saved = data_transformation(input_df)
+    data_to_be_saved = get_some_df(spark_session)
 
     table_name = f"full_electricity_{uuid4().hex.upper()[:6]}"
 
